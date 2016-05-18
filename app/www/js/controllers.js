630b65cf1e27d60e637019013fe16953fc9f9587
Sven de Ronde <ewderonde@gmail.com>
2016-05-18 15:55:28 +0200

    Producten worden dynamisch ingeladen (De laatste 2 items moeten qua css nog gefixed worden)

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -41,11 +41,31 @@ angular.module('starter.controllers', [])
    * kan verwerken in een facory (net zoals bij chats gedaan is) dit is namelijk wel een
    * mooie usecase voor een factory (services.js en tab-chats + check de routes file)
    */
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat ' },
-    { title: 'Get himushroomsgh enough to grab the flag' },
-    { title: 'Find the Princess' }
+
+
+  // Heb de prijs even 2 keer toegevoegd, omdat integers bij de front end worden afgerond en hoef op
+  // deze manier later geen eventuele regexes toe te passen. :)
+  $scope.products = [
+    { title: 'Gouda Kaas 48+', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2kg", thumbnail:"img/goudakaas.JPG"},
+    { title: 'Calvé Pindakaas', prijsFrontEnd: "2,30", prijs: 2.30, inhoud: "350 g", thumbnail:"img/pindakaas.JPG"},
+    { title: 'Quaker Havermout', prijsFrontEnd: "3,50", prijs: 3.50, inhoud: "550 g", thumbnail:"img/havermout.JPG"},
+    { title: 'Coca Cola', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/cocacola.JPG" },
+    { title: 'Kip Cilet', prijsFrontEnd: "3,50", prijs: 4.50, inhoud: "1 kilo", thumbnail:"img/kipfilet.JPG" },
+    { title: 'La Chouffe', prijsFrontEnd: "2,45", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/lachouffe.JPG" },
+    { title: 'Big Americans pizza', prijsFrontEnd: "2.95", prijs: 2.95, inhoud: "435 g", thumbnail:"img/pizza.JPG" },
+    { title: 'AH Frambozenvla', prijsFrontEnd: "1,05", prijs: 1.05, inhoud: "1 liter", thumbnail:"img/frambozenvla.JPG" },
+
+    // Dit hieronder is even copy pasta werk, kan later weer weg.
+    { title: 'Coca Cola', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/cocacola.JPG" },
+    { title: 'Kip Cilet', prijsFrontEnd: "3,50", prijs: 4.50, inhoud: "1 kilo", thumbnail:"img/kipfilet.JPG" },
+    { title: 'La Chouffe', prijsFrontEnd: "2,45", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/lachouffe.JPG" },
+    { title: 'Big Americans pizza', prijsFrontEnd: "2.95", prijs: 2.95, inhoud: "435 g", thumbnail:"img/pizza.JPG" },
+    { title: 'AH Frambozenvla', prijsFrontEnd: "1,05", prijs: 1.05, inhoud: "1 liter", thumbnail:"img/frambozenvla.JPG" },
+    { title: 'Coca Cola', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/cocacola.JPG" },
+    { title: 'Kip Cilet', prijsFrontEnd: "3,50", prijs: 4.50, inhoud: "1 kilo", thumbnail:"img/kipfilet.JPG" },
+    { title: 'La Chouffe', prijsFrontEnd: "2,45", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/lachouffe.JPG" },
+    { title: 'Big Americans pizza', prijsFrontEnd: "2.95", prijs: 2.95, inhoud: "435 g", thumbnail:"img/pizza.JPG" },
+    { title: 'AH Frambozenvla', prijsFrontEnd: "1,05", prijs: 1.05, inhoud: "1 liter", thumbnail:"img/frambozenvla.JPG" }
   ];
   
   console.log("kaas");
