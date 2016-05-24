96de449fa516ec17d2f9810a492acdf604524a70
Sven de Ronde <ewderonde@gmail.com>
2016-05-24 13:36:27 +0200

    kleine fix

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -39,7 +39,8 @@ angular.module('starter.controllers', ['ionic', 'ionic.contrib.ui.tinderCards'])
     ShoppingList.remove(item);
   };
 
-  $scope.add = function(){
+  $scope.add = function(e){
+    console.log(e);
     var name = document.getElementById("productInput");
     var aantal = document.getElementById("aantalInput");
     if(name.value != "" && aantal.value != "Kies aantal")
