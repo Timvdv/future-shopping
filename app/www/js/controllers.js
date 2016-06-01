5c1abd01097f6a3f4f05f309d0d27d69202a7703
Sven de Ronde <ewderonde@gmail.com>
2016-06-01 21:05:44 +0200

    er is nu een route waarmee je de localstorage leegmaakt en gelijk weer vult met dummydata. /data om de localstorage te vullen

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -33,6 +33,41 @@ angular.module('starter.controllers', ['ionic', 'ionic.contrib.ui.tinderCards'])
   };
 })
 
+.controller('AddDataCtlr', function($scope, $stateParams, $location, $state) {
+  console.log("Added product data to localStorage.");
+  localStorage.clear();
+  var products = [
+    { id: 1, title: 'Gouda Kaas 48+', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2kg", thumbnail:"img/goudakaas.JPG", aantal: 1, inpakTijd: 10},
+    { id: 2, title: 'Calvé Pindakaas', prijsFrontEnd: "2,30", prijs: 2.30, inhoud: "350 g", thumbnail:"img/pindakaas.JPG", aantal: 1 , inpakTijd: 8}, 
+    { id: 3, title: 'Quaker Havermout', prijsFrontEnd: "3,50", prijs: 3.50, inhoud: "550 g", thumbnail:"img/havermout.JPG", aantal: 1, inpakTijd: 15},
+    { id: 4, title: 'Coca Cola', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/cocacola.JPG", aantal: 1, inpakTijd: 16 },
+    { id: 5, title: 'Kip Filet', prijsFrontEnd: "3,50", prijs: 4.50, inhoud: "1 kilo", thumbnail:"img/kipfilet.JPG", aantal: 1, inpakTijd: 20 },
+    { id: 6, title: 'La Chouffe', prijsFrontEnd: "2,45", prijs: 4.50, inhoud: "2 liter", thumbnail:"img/lachouffe.JPG", aantal: 1, inpakTijd: 13 },
+    { id: 7, title: 'Big Americans pizza', prijsFrontEnd: "2.95", prijs: 2.95, inhoud: "435 g", thumbnail:"img/pizza.JPG", aantal: 1, inpakTijd: 11 },
+    { id: 8, title: 'AH Frambozenvla', prijsFrontEnd: "1,05", prijs: 1.05, inhoud: "1 liter", thumbnail:"img/frambozenvla.JPG", aantal: 1, inpakTijd: 12 }
+  ];
+
+  var favorites = [
+    { id: 1, title: 'Gouda Kaas 48+', prijsFrontEnd: "4,50", prijs: 4.50, inhoud: "2kg", thumbnail:"img/goudakaas.JPG", aantal: 1, inpakTijd: 10},
+    { id: 2,title: 'Calvé Pindakaas', prijsFrontEnd: "2,30", prijs: 2.30, inhoud: "350 g", thumbnail:"img/pindakaas.JPG", aantal: 1 , inpakTijd: 8}, 
+    { id: 3,title: 'Quaker Havermout', prijsFrontEnd: "3,50", prijs: 3.50, inhoud: "550 g", thumbnail:"img/havermout.JPG", aantal: 1, inpakTijd: 15}
+  ];
+
+  var shoppingList = [
+    {title: "Gouda Kaas 48+", aantal: 1, checked: false}, 
+    {title: "Quaker Havermout", aantal: 2, checked: false}, 
+    {title: "Calvé Pindakaas", aantal: 1, checked: true}, 
+    {title: "AH Frambozenvla", aantal: 1, checked: false}
+  ];
+
+  localStorage["products"] = JSON.stringify(products);
+  localStorage["favorites"] = JSON.stringify(favorites);
+  localStorage["shoppingList"] = JSON.stringify(shoppingList);
+
+  window.location.reload(true);  
+  $location.path('#/tab/dash');
+})
+
 .controller('ShoppingListCtrl', function($scope, $stateParams, ShoppingList, $ionicPopup, $timeout) {
   $scope.list = ShoppingList.all();
   $scope.remove = function(item) {
