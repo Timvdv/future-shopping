2151733d302572fc21b481ab8af6211aa88cc54f
Tim van der Hoek <timvanderhoek1994@gmail.com>
2016-06-02 12:32:44 +0200

    tutorial fixes ios

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -349,14 +349,11 @@ angular.module('starter.controllers', ['ionic', 'ionic.contrib.ui.tinderCards'])
 // function secondsToTime(secs)
 // {
 //     secs = Math.round(secs);
-//     var hours = Math.floor(secs / (60 * 60));
-// ​
+//     var hours = Math.floor(secs / (60 * 60)); ​
 //     var divisor_for_minutes = secs % (60 * 60);
-//     var minutes = Math.floor(divisor_for_minutes / 60);
-// ​
+//     var minutes = Math.floor(divisor_for_minutes / 60); ​
 //     var divisor_for_seconds = divisor_for_minutes % 60;
 //     var seconds = Math.ceil(divisor_for_seconds);
-// ​
 //     var obj = {
 //         "h": hours,
 //         "m": minutes,
