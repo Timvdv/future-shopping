566c13a74b7a36a1d8574983d8a2e7f10fa4b3c9
Casper Streefkerk <Casper@145-24-227-106.wlan.hro.nl>
2016-06-21 14:27:32 +0200

    Prijs laten zien in het checkout scherm (local storage moet nog het aantal aanpassen)

--- app/www/js/controllers/CheckoutCtrl.js
+++ app/www/js/controllers/CheckoutCtrl.js
@@ -1,12 +1,30 @@
-angular.module('starter.controllers').controller('CheckoutCtrl', ['$scope', '$http', '$location' , function($scope, $http, $location)
+angular.module('starter.controllers').controller('CheckoutCtrl', ['$scope', '$http', '$location' , 'Products', function($scope, $http, $location, Products)
 {
 	console.log("Checkout Controller. GO HAM CASPER!");
 
 	// var totalprice = 0;
+	$scope.products = Products.all();
+	$scope.totalPrice = 0;
 
-	// //ophalen van totale prijs uit vorige view
-	// scope.getTotalPrice = function() {
+	// Bereken de nieuwe totale prijs & de inpaktijd.
+    $scope.calculateData = function() 
+    {
+        var totalPrice = 0;
+        var totalTime = 0
+        for (var i = 0; i < $scope.products.length; i++) {
+            totalPrice += ($scope.products[i].aantal * $scope.products[i].prijs);
+            totalTime += ($scope.products[i].aantal * $scope.products[i].inpakTijd);
+        }
 
-	// }
+        $scope.totalPrice = Math.round(totalPrice * 100) / 100;
+        //$scope.totalTime = secondsToTime(totalTime);
+
+        $scope.totalPrice = $scope.totalPrice.toFixed(2).split('.')[0] + ',' + $scope.totalPrice.toFixed(2).split('.')[1]
+        //document.getElementById('price').innerHTML =  $scope.totalPrice;
+        //$scope.updateTotals();
+    }
+
+    // Berekenen wat de totale prijs is, de eerste keer dat de pagina geladen wordt.
+    $scope.calculateData();
 
 }]);
